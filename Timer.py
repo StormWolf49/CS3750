82c258a250b631537ef96a8af19c6f1cc4dd1b3c
Nikhil Kowdle <72424123+StormWolf49@users.noreply.github.com>
2023-09-07 13:17:21 -0700

    Create Timer.py

---
+++ Timer.py
@@ -0,0 +1,25 @@
+ # import the time module
+import time
+dilation = 2
+
+# define the countdown func.
+def countdown(t):
+	print()
+
+	while t:
+		Rmins, Rsecs = divmod(t, 60)
+		Rtimer = '{:02d}:{:02d}'.format(Rmins, Rsecs)
+		Gmins, Gsecs = divmod(t//dilation, 60)
+		Gtimer = '{:02d}:{:02d}'.format(Gmins, Gsecs)
+		print("Real Time:", Rtimer, "     ", "Game Time:", Gtimer, end="\r")
+		time.sleep(1)
+		t -= 1
+	
+	print('               Time\'s up               ')
+
+
+# input time in seconds
+t = dilation*int(input("Enter the time in seconds: "))
+
+# function call
+countdown(t)
